a6a4721a368e7a858b040c90f862826c19258473
dow99999 <38563257+dow99999@users.noreply.github.com>
2020-06-24 17:39:08 +0200

    commit suicide ( testing host in raspberry )

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+//imports
 const Discord = require("discord.js");
+const config = required("config.json");
+
+//declaracion variables
 const client = new Discord.Client();
 
-client.on("ready", () => {});
+//init variables
+client.on("ready", () => {
+    console.log("UwU");
+});
 
-client.on("message", () => {});
+//UwU messages
+client.on("message", (message) => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-client.login("");
+    if (message.content.startsWith(config.prefix + "ping")) {
+        message.channel.send("pong!");
+    }
+});
+
+client.login(config.token);
