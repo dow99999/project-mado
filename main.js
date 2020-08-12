7bb2be0464dc18b467e99f1b20a8c9fc41157bac
MaaximusCP <67378341+MaaximusCP@users.noreply.github.com>
2020-08-12 19:00:46 +0200

    inicio raspberry y dejar bonito

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //imports
 const Discord = require("discord.js");
 const config = require("./config.json");
+const copypasta = require("./data/copypastas.json");
 
 //declaracion variables
 const client = new Discord.Client();
@@ -8,25 +9,49 @@ var comando;
 var args;
 
 
-//declaracion de funciones
+//declaracion de funciones/////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
  * envia un mensaje en el canal en el cual se ha escrito el mensaje de los argumentos que tiene el comando
  */
-function repite(f_message, f_args){
+function repite(f_args){
     let lafrase = "\n-->";
     for(let i = 1; i < f_args.length; i -=- 1){
        lafrase += f_args[i]+ "\t";
     }
-    f_message.channel.send("Habars dicho..." + lafrase);
+
+    return ("Habars dicho..." + lafrase);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function comandos(f_comando, f_args){
+    switch(f_comando){
+        case "repite":
+            message.channel.send(repite(f_args));
+            break;
+        case "ChingChong":
+            message.channel.send('North Korea stronk');
+            break;
+    }
 }
 
 
 
-
-function autoresponder(f_comando){
-
+function autoresponder(f_comando, f_args){
+    switch(f_comando){
+        case "repite":
+            message.channel.send(repite(f_args));
+            break;
+        case "ChingChong":
+            message.channel.send('North Korea stronk');
+            break;
+    }
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //init variables
 client.on("ready", () => {
@@ -35,53 +60,32 @@ client.on("ready", () => {
 
 //UwU messages
 client.on("message", (message) => {
-
-    //comando = message.content.slice(config.prefix.length - 1, message.content.length);
     
-    if (!message.author.bot) {                                            //Comprovacion de humano
+    if (!message.author.bot) {                                                             //Comprovacion de humano
 
-        if (message.content.startsWith(config.prefix)) {                 //mensajes con prefijo
-            comando = message.content.slice(config.prefix.length, message.content.length);
-            args = comando.split(" "); //usar a partir del 0
-            comando = args [0]; 
+        if (message.content.startsWith(config.prefix)) {                                   //mensajes con prefijo
+                                                                                           //incializacion de mensaje
+            comando = message.content.slice(config.prefix.length, message.content.length); //borramos el prefijo
+            args = comando.split(" ");                                                     //argumentos detras del comando separados por espacios **usar a partir del 0**
+            comando = args [0];                                                            //comando(palabra sin espacios) sin su prefijo
 
-            //TODO: split del prefijo para mejor comprobacion
-            console.log("mensaje: " + message.content + 
-            "\ncomando: " + comando + 
-            "\nargs" +  args
-            );
-
-            //Comandos ////////////////////////////////////////////////////////////////////////////
-            switch(comando){
-                case "repite":
-                    repite(message, args);
-                    break;
-                case "ChingChong":
-                    message.channel.send('North Korea stronk');
-                    break;
-            }
+            //Comandos ////////////////////////////////////////////////////////////////////////////////////////////////
             
+            comandos(comando, args);
 
-           ///////////////////////////////////////////////////////////////////////////////////////
-        } else {                                                                //SIN PREFIJO
-            //incializacion de mensaje
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+        } else {                                                                           //SIN PREFIJO
+                                                                                           //incializacion de mensaje
             comando = message.content;
-            args = comando.split(" "); //usar a partir del 0
-            comando = args[0];
+            args = comando.split(" ");                                                     //argumentos detras del comando separados por espacios **usar a partir del 0**
+            comando = args[0];                                                             //comando(palabra sin espacios)
 
-            //AUTORESPUESTAS/////////////////////////////////////////////////////////////////////////
+            //AUTORESPUESTAS///////////////////////////////////////////////////////////////////////////////////////////
 
-            switch(comando){
-                case "repite":
-                    repite(message, args);
-                    break;
-                case "ChingChong":
-                    message.channel.send('North Korea stronk');
-                    break;
-            }
+            autoresponder(comando, args);
             
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
         }
-            ////////////////////////////////////////////////////////////////////////////////////////
     }
 
 });
